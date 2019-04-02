7c2b76346240ed8bf65e6e8029130b6ce62c0645
RedDucks <jonbarrow1998@gmail.com>
2019-04-02 16:56:55 -0400

    Removed stuff that isn't needed
    A lot of stuff was left over from the example controls but we didn't use them

--- lib/widgets/video_player_controls.dart
+++ lib/widgets/video_player_controls.dart
@@ -3,8 +3,6 @@
 */
 
 import 'dart:async';
-import 'dart:math' as math;
-import 'dart:ui' as ui;
 
 import 'package:chewie/src/chewie_player.dart';
 import 'package:chewie/src/chewie_progress_colors.dart';
@@ -32,7 +30,6 @@ class MiruVideoPlayerControls extends StatefulWidget {
 
 class _MiruVideoPlayerControlsState extends State<MiruVideoPlayerControls> {
   VideoPlayerValue _latestValue;
-  double _latestVolume;
   bool _hideStuff = true;
   Timer _hideTimer;
   final marginSize = 5.0;
@@ -44,13 +41,10 @@ class _MiruVideoPlayerControlsState extends State<MiruVideoPlayerControls> {
 
   @override
   Widget build(BuildContext context) {
-    final backgroundColor = widget.backgroundColor;
-    final iconColor = widget.iconColor;
     chewieController = ChewieController.of(context);
     controller = chewieController.videoPlayerController;
     final orientation = MediaQuery.of(context).orientation;
     final barHeight = orientation == Orientation.portrait ? 30.0 : 47.0;
-    final buttonPadding = orientation == Orientation.portrait ? 16.0 : 24.0;
 
     return GestureDetector(
       onTap: () {
@@ -84,15 +78,6 @@ class _MiruVideoPlayerControlsState extends State<MiruVideoPlayerControls> {
 
   @override
   void didChangeDependencies() {
-    final _oldController = chewieController;
-    chewieController = ChewieController.of(context);
-    controller = chewieController.videoPlayerController;
-
-    if (_oldController != chewieController) {
-      _dispose();
-      _initialize();
-    }
-
     super.didChangeDependencies();
   }
 
@@ -134,48 +119,6 @@ class _MiruVideoPlayerControlsState extends State<MiruVideoPlayerControls> {
             ),
           ),
         )
-      ),
-    );
-  }
-
-
-  GestureDetector _buildExpandButton(
-    Color backgroundColor,
-    Color iconColor,
-    double barHeight,
-    double buttonPadding,
-  ) {
-    return GestureDetector(
-      onTap: _onExpandCollapse,
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 300),
-        child: ClipRect(
-          child: BackdropFilter(
-            filter: ui.ImageFilter.blur(sigmaX: 10.0),
-            child: Container(
-              height: barHeight,
-              padding: EdgeInsets.only(
-                left: buttonPadding,
-                right: buttonPadding,
-              ),
-              decoration: BoxDecoration(
-                color: backgroundColor,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10.0),
-                ),
-              ),
-              child: Center(
-                child: Icon(
-                  chewieController.isFullScreen
-                      ? OpenIconicIcons.fullscreenExit
-                      : OpenIconicIcons.fullscreenEnter,
-                  color: iconColor,
-                  size: 12.0,
-                ),
-              ),
-            ),
-          ),
-        ),
       ),
     );
   }
@@ -333,36 +276,6 @@ class _MiruVideoPlayerControlsState extends State<MiruVideoPlayerControls> {
     });
   }
 
-  Future<Null> _initialize() async {
-    controller.addListener(_updateState);
-
-    _updateState();
-
-    if ((controller.value != null && controller.value.isPlaying) ||
-        chewieController.autoPlay) {
-      _startHideTimer();
-    }
-
-    _initTimer = Timer(Duration(milliseconds: 200), () {
-      setState(() {
-        _hideStuff = false;
-      });
-    });
-  }
-
-  void _onExpandCollapse() {
-    setState(() {
-      _hideStuff = true;
-
-      chewieController.toggleFullScreen();
-      _expandCollapseTimer = Timer(Duration(milliseconds: 300), () {
-        setState(() {
-          _cancelAndRestartTimer();
-        });
-      });
-    });
-  }
-
   Widget _buildProgressBar() {
     return Expanded(
       child: Padding(
@@ -405,20 +318,6 @@ class _MiruVideoPlayerControlsState extends State<MiruVideoPlayerControls> {
         }
       }
     });
-  }
-
-  void _skipBack() {
-    _cancelAndRestartTimer();
-    final beginning = Duration(seconds: 0).inMilliseconds;
-    final skip = (_latestValue.position - Duration(seconds: 15)).inMilliseconds;
-    controller.seekTo(Duration(milliseconds: math.max(skip, beginning)));
-  }
-
-  void _skipForward() {
-    _cancelAndRestartTimer();
-    final end = _latestValue.duration.inMilliseconds;
-    final skip = (_latestValue.position + Duration(seconds: 15)).inMilliseconds;
-    controller.seekTo(Duration(milliseconds: math.min(skip, end)));
   }
 
   void _startHideTimer() {
