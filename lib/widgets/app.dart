9e0aa289f15e6083ab356ef7e4b4d16d6f6caf1a
mrjvs <jellevs@gmail.com>
2019-05-07 19:48:15 +0200

    somewhat working transitions with a lot of comment

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -16,16 +16,99 @@ class MiruApp extends StatefulWidget {
   }
 }
 
-class MiruAppState extends State<MiruApp> {
-  
+class MiruAppState extends State<MiruApp> with TickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> tabOffset;
+
   @override
   void initState() {
     super.initState();
+
+    if (previous < index) {
+      goRight = false;
+    } else {
+      goRight = true;
+    }// transition direction
+
+    controller = AnimationController( // animation controller handles all animations
+      duration: const Duration(milliseconds: 500), // duration of the transition
+      vsync: this
+    )..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+          setState(() {
+            animationInProgress = false; // setting this to false removes all of the animation offsets to get ready for the next transition.
+          });
+      }
+    });
+
+    tabOffset = Tween<double>( // tabboffset gets animated from 0 to 1 if animating.
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.ease
+    ))..addListener(() {
+        setState(() {}); // setting state so stuff actually gets updated.
+    });
   }
 
-  int index = 1;
+  void changeTab(int newIndex) {
+    setState(() {
+      animationInProgress = true;
+      previous = index;
+      index = newIndex;
+
+      if (previous < index) {
+        goRight = false;
+      } else {
+        goRight = true;
+      }// transition direction
+      
+    });
+    controller.forward();
+  }
+
+  double calculateOffset(int tab, double width) {
+    // in animation
+    if (animationInProgress) {
+      if (tab == previous) {
+        if (goRight) {
+          // if the tab should be pushed to the right out of view
+          return width * tabOffset.value;
+        } else {
+          // if the tab should be pushed to the left out of view
+          return width * tabOffset.value * -1; // inverted so it goes left
+        }
+      }
+      if (tab == index) {
+        if (goRight) {
+          // if the tab should be pushed to the right into of view
+          return width * (tabOffset.value -1 ); // offsetted by one so it goes from outside of the view to inside
+        } else {
+          // if the tab should be pushed to the left into of view
+          return width * ((tabOffset.value *-1) + 1); // inverted so it goes left and offsetted by one so it goes from outside the view to inside
+        }
+      }
+    } else {
+      return 0;
+    }
+    // tab shouldn't be rendered.
+    return 0;
+  }
+
+  bool shouldRender(int tab) {
+    if (tab == previous && animationInProgress) return true;
+    if (tab == index) return true;
+    return false;
+  }
+
+  int index = 3;
+  int previous = 0;
+  bool goRight = false;
+  bool animationInProgress = false;
   @override
   Widget build(BuildContext ctx) {
+    final double width = MediaQuery.of(context).size.width;
     return DecoratedBox(
       decoration: BoxDecoration(
         color: MiruColors.dark
@@ -37,56 +120,71 @@ class MiruAppState extends State<MiruApp> {
               child: Stack(
                 children: <Widget>[
                   Offstage(
-                    offstage: index != 1,
+                    offstage: !shouldRender(1),
                     child: TickerMode(
-                      enabled: index == 1,
-                      child: HomeTab(
-                        appContext: ctx
-                      ),
-                    ),
-                  ),
-                  Offstage(
-                    offstage: index != 2,
-                    child: TickerMode(
-                      enabled: index == 2,
-                      child: SearchTab(
-                        appContext: ctx
+                      enabled: shouldRender(1),
+                      child: Transform.translate(
+                        offset: Offset(calculateOffset(1, width),0.0),
+                        child: HomeTab(
+                          appContext: ctx
+                        ),
                       )
                     ),
                   ),
                   Offstage(
-                    offstage: index != 3,
+                    offstage: !shouldRender(2),
                     child: TickerMode(
-                      enabled: index == 3,
-                      child: LibraryTab(
-                        appContext: ctx
+                      enabled: shouldRender(2),
+                      child: Transform.translate(
+                        offset: Offset(calculateOffset(2, width),0.0),
+                        child: SearchTab(
+                          appContext: ctx
+                        ),
                       )
                     ),
                   ),
                   Offstage(
-                    offstage: index != 4,
+                    offstage: !shouldRender(3),
                     child: TickerMode(
-                      enabled: index == 4,
-                      child: SettingsTab(
-                        appContext: ctx
+                      enabled: shouldRender(3),
+                      child: Transform.translate(
+                        offset: Offset(calculateOffset(3, width),0.0),
+                        child: LibraryTab(
+                          appContext: ctx
+                        ),
+                      )
+                    ),
+                  ),
+                  Offstage(
+                    offstage: !shouldRender(4),
+                    child: TickerMode(
+                      enabled: shouldRender(4),
+                      child: Transform.translate(
+                        offset: Offset(calculateOffset(4, width),0.0),
+                        child: SettingsTab(
+                          appContext: ctx
+                        ),
                       )
                     ),
                   ),
                   Offstage( // testing tab
-                    offstage: index != 5,
+                    offstage: !shouldRender(5),
                     child: TickerMode(
-                      enabled: index == 5,
-                      child: SearchTab()
+                      enabled: shouldRender(5),
+                      child: Transform.translate(
+                        offset: Offset(calculateOffset(5, width),0.0),
+                        child: SearchTab(
+                          appContext: ctx
+                        ),
+                      )
                     ),
                   ),
                 ],
               )
             ),
             BottomBar(
-              onTap: (int pageIndex) => {
-                setState(() {
-                  index = pageIndex;
-                })
+              onTap: (int pageIndex) {
+                changeTab(pageIndex);
               },
               selected: this.index
             )
